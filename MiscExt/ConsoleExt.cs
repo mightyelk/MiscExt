7ebdac6c793132670271909188a9bed2221d1ba5
MightyElk <mightyelk@gmail.com>
2022-11-16 16:10:43 +0100

    colorful console

---
+++ MiscExt/ConsoleExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MightyElk.MiscExt
+{
+    public static class ConsoleExt
+    {
+        public static void Write(ConsoleColor color, string format, params object[] args)
+        {
+            var oldCol = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(format, args);
+            Console.ForegroundColor = oldCol;
+        }
+
+        public static void WriteLine(ConsoleColor color, string format, params object[] args)
+        {
+            Write(color, format+Environment.NewLine, args);
+        }
+
+        public static void WriteLine(ConsoleColor color, string text)
+        {
+            WriteLine(color, "{0}", text);
+        }
+    }
+}
